c44189fccf664285c7e095438cf397291e7ec83e
9gods <codingcomelli@gmail.com>
2025-06-11 23:35:50 -0300

    feat: add riverpod providerscope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'src/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const DarfyApp());
+  runApp(const ProviderScope(child: DarfyApp()));
 }
